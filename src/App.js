02d834a8434d967047ec93fde1e7d2fc5a509c09
han1822815550 <1822815550@qq.com>
2018-08-16 23:44:05 +0800

    完成首页及计算器功能

--- src/App.js
+++ src/App.js
@@ -13,14 +13,17 @@ import axios from 'axios';
 // axios.defaults.baseURL = 'http://47.96.21.88:8086/';
 axios.defaults.baseURL = 'http://127.0.0.1:8086/';
 
-// Add a request interceptor
-// axios.interceptors.request.use(function (config) {
-//     // Do something before request is sent
-//     return config;
-//   }, function (error) {
-//     // Do something with request error
-//     return Promise.reject(error);
-//   });
+// 请求拦截器
+axios.interceptors.request.use(function (config) {
+  if (!config.url.endsWith('/login')) {
+    // 登录的地址不需要添加token请求头
+    config.headers.Authorization = localStorage.getItem('mytoken');
+  }
+  return config;
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error);
+});
 
 // 响应拦截器
 axios.interceptors.response.use(function (response) {
