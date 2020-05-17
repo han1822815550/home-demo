300d6bd1a3c45d8985931d29a73f7bd9727a4599
hanchongyun <15550502620@163.com>
2020-05-17 22:46:04 +0800

    测试格式化代码

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirectß
 } from "react-router-dom";
 
 import './App.css';
@@ -15,43 +15,43 @@ axios.defaults.baseURL = 'http://127.0.0.1:8086/';
 
 // 请求拦截器
 axios.interceptors.request.use(function (config) {
-  if (!config.url.endsWith('/login')) {
-    // 登录的地址不需要添加token请求头
-    config.headers.Authorization = localStorage.getItem('mytoken');
-  }
-  return config;
+    if (!config.url.endsWith('/login')) {
+        // 登录的地址不需要添加token请求头
+        config.headers.Authorization = localStorage.getItem('mytoken');
+    }
+    return config;
 }, function (error) {
-  // Do something with request error
-  return Promise.reject(error);
+    // Do something with request error
+    return Promise.reject(error);
 });
 
 // 响应拦截器
 axios.interceptors.response.use(function (response) {
-  // Do something with response data
-  // console.log(response)
-  return response.data
+    // Do something with response data
+    // console.log(response)
+    return response.data
 }, function (error) {
-  // Do something with response error
-  return Promise.reject(error);
+    // Do something with response error
+    return Promise.reject(error);
 });
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Login} />
-          <Route path="/home" component={Main} />
-          <Route path="/show" component={Show} />
-          <Redirect to="/" />
-        </Switch>
-      </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Login} />
+                    <Route path="/home" component={Main} />
+                    <Route path="/show" component={Show} />
+                    <Redirect to="/" />
+                </Switch>
+            </Router>
+        );
+    }
 }
 
 const Show = () => {
-  return <div>Show</div>
+    return <div>Show</div>
 }
 
 export default App;
